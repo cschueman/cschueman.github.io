50b263c30443ea2b161314d84ce445f0475c8e14
cschueman <cschueman@eagles.ewu.edu>
2017-12-07 13:20:25 -0800

    random heading generator
    hey look at that it works

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,14 @@
-// Randomize Header
+// 1. page load 2. store headers 3. ... x. Show header
+
+// Store headers
+var header0 = 'hi';
+var header1 = 'hi there';
+var header2 = 'welcome';
 
 // Store & Generate Number
 var maxRand = 3;
-var numRand = Math.floor( Math.random() * maxRand );
-//console.log(numRand);
+var randNum = Math.floor( Math.random() * maxRand );
+console.log(randNum);
 
-// Store images
-var img0 = 'http://placehold.it/800x400';
-var img1 = 'http://placehold.it/800x400';
-var img2 = 'http://placehold.it/800x400';
-
-// Show Images
-$('header img').attr('src', eval('img' + numRand) );
-
-// Activate Gallery
-$('figure').lightGallery({
-    selector: 'a', 
-    thumbnail: true,
-});
-
-$('figure').lightGallery({
-  selector: 'a',
-  mode: 'lg-fade',
-  download: false,
-  speed: 100,
-  share: false,
-  autoplay: false,
-});
+//  Show headers
+$('h1').text( eval('header' + randNum) );
